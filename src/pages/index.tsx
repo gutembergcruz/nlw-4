77a6cb38201170a91d3a17e32502a923441f837b
Gutemberg <gute.tec@gmail.com>
2021-02-26 19:32:53 -0300

    countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,26 @@
 import Head from 'next/head';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+
+import head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <ExperienceBar />
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
 
+      <ExperienceBar />
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
